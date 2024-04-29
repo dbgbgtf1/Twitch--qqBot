842de10f69b791eb48402bdd00b30ca46d7ab199
dbgbgtf <149954065+dbgbgtf1@users.noreply.github.com>
2024-04-29 15:44:53 +0800

    Update bili.py

--- bili.py
+++ bili.py
@@ -2,8 +2,7 @@ import json
 import logging
 from bilibili_api import user,sync
 import asyncio
-
-#用文件操作记录每个up主最新视频的bv号，并且每次获取最新视频bv号与文件内的bv号对比
+# 实例化
 def write_file(filename, content):
     with open(filename, 'w') as file:
         file.write(content)
@@ -22,12 +21,11 @@ def compare_file(filename,target_string):
             return True
 
 async def get_up_video(up_list):
-#遍历每个up主，检查其视频bv号
     for up_name,up_uid in up_list.items():
-
         up = user.User(uid = up_uid)
-        #用了bilibili-api的模块获取up最新视频
+
         media = await up.get_media_list(ps = 1)
+
         bv_id = media.get("media_list")[0].get("bv_id")
         logging.warning('bv_id:' + bv_id)
         #print(json.dumps(media,indent = 2,ensure_ascii = False))
@@ -37,21 +35,19 @@ async def get_up_video(up_list):
             media_link = media.get("media_list")[0].get("short_link")
             write_file(up_name,bv_id)
             logging.warning(f"{up_name}发布了新视频!")
-            logging.warning(f'media_title:{media_title}')
-            logging.warning(f'media_link:{media_link}\n')
+            logging.warning(f'视频标题:{media_title}')
+            logging.warning(f'视频链接:{media_link}\n')
             return up_name,media_title,media_link
-
         else:
             logging.warning(f"{up_name} no new video\n")
-            await asyncio.sleep(10)
-    return False,False,False
+    return "False","False","False"
 
 if __name__ == "__main__":
+    print("testing bili.py ...")
     up_list = {}
     up_list['小小小Janey'] = 32149224
     up_list['TheOnlyShark'] = 517913954
     up_list['COMAYUMI'] = 14445191
     up_list['HeNTa1111'] = 1114874220
     up_list['sunshine102506'] = 3537124194257576
-    #欢迎来看看这几个up的视频hhh
     sync(get_up_video(up_list))
